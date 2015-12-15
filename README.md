12e2612b9b606b0dc0dbbf488bd809353aad9932
Graeme Coupar <grambo@grambo.me.uk>
2015-12-15 13:56:45 +0000

    Add note to README & config details output.

--- README.md
+++ README.md
@@ -18,6 +18,31 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
           [applications: [:vassal]]
         end
 
+## Implemented Actions
+
+- CreateQueue
+- GetQueueUrl
+- DeleteQueue
+- SendMessage
+- ReceiveMessage
+- DeleteMessage
+- ChangeMessageVisibility
+- SetQueueAttributes
+- GetQueueAttributes
+
+## Missing Features
+
+Some of these may be implemented in the near future:
+
+- User defined message attributes.
+- Getting non-config queue attributes (other than ARN).
+  ApproximateNumberOfMessages etc. are not supported.
+- Listing queues
+- Authentication / permissions, or anything related.
+- MD5 checksums.
+- SenderId Attribute on messages.
+- Persistence.  All data & queues will be lost on restart.
+
 ## Existing Works
 
 This is not the first queing server to implement an SQS interface.  There is
--- lib/vassal.ex
+++ lib/vassal.ex
@@ -2,13 +2,17 @@ defmodule Vassal do
   @moduledoc """
   A simple message queue with an SQS interface.
   """
-
   use Application
+
+  require Logger
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+
+    conn_details = [port: Application.get_env(:vassal, :port),
+                    ip: Application.get_env(:vassal, :ip)]
 
     children = [
       worker(Vassal.QueueStore, []),
@@ -18,11 +22,11 @@ defmodule Vassal do
          [strategy: :simple_one_for_one, name: Vassal.QueueSupervisor]]
       ),
       Plug.Adapters.Cowboy.child_spec(
-        :http, Vassal.WebRouter, [],
-        [port: Application.get_env(:vassal, :port),
-         ip:   Application.get_env(:vassal, :ip)]
+        :http, Vassal.WebRouter, [], conn_details
       )
     ]
+
+    Logger.info("Config: #{inspect conn_details}")
 
     opts = [strategy: :one_for_one, name: Vassal.Supervisor]
     Supervisor.start_link(children, opts)
