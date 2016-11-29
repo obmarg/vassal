ce0b0f7bac59e79be5bd46bd47fc3e43d2071cbd
Graeme Coupar <grambo@grambo.me.uk>
2016-11-29 20:31:28 +0000

    Update to use Elixir 1.3
    Not actually using any new features from it, just pulling in the dependencies
    etc.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vassal.Mixfile do
   def project do
     [app: :vassal,
      version: "0.2.2",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
