2b2572fc715d6b374a3c5475656f4c16f40adec0
Graeme Coupar <grambo@grambo.me.uk>
2016-12-06 18:51:19 +0000

    Fix pre-start.sh on linux.
    - base64 wraps by default on linux.  There's a command line argument to
      disable this, but the mac version does not support this.  I'm just
      joining together with awk instead, which should be platform agnostic.
    - The `-i` argument to sed works slightly different on linux: it doesn't
      seem to support a space between the extension and the `-i`.
      Thankfully the mac version is fine with that too, so I've gone for
      that.

--- rel/hooks/pre-start.sh
+++ rel/hooks/pre-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Initializing secure erlang distribution cookie..."
-NEW_COOKIE=$(dd if=/dev/urandom bs=1 count=128 2>/dev/null | base64 | sed -e 's/\///g')
-sed -e "s/^-setcookie.*/-setcookie $NEW_COOKIE/" -i "insecure" "$VMARGS_PATH"
+NEW_COOKIE=$(dd if=/dev/urandom bs=1 count=128 2>/dev/null | base64 | awk '!/0$/{printf $0}/0$/' | sed -e 's/\///g')
+sed -e "s/^-setcookie.*/-setcookie $NEW_COOKIE/" -i".insecure" "$VMARGS_PATH"
