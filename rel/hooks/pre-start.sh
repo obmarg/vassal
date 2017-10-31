d4388320e3b8fa54a9a4de92e770110d8af7bbef
Graeme Coupar <grambo@grambo.me.uk>
2017-10-31 17:20:47 +0000

    Pre-start hook should support python3 as well as 2.

--- rel/hooks/pre-start.sh
+++ rel/hooks/pre-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Initializing secure erlang distribution cookie..."
-NEW_COOKIE=$(python -c "import base64; import os; print(base64.urlsafe_b64encode(os.urandom(64)).replace('/', '_'))")
+NEW_COOKIE=$(python -c "import base64; import os; rand = os.urandom(64); print(base64.urlsafe_b64encode(os.urandom(64)).replace(b'/', b'_'))")
 sed -e "s/^-setcookie.*/-setcookie $NEW_COOKIE/" -i".insecure" "$VMARGS_PATH"
